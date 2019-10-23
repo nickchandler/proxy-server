4d3df0cf77eb5227a09450ec81af0534a5adc6f6
nick.chand.20@gmail.com <Nick@Nicholass-MacBook-Pro.local>
2019-10-22 20:48:32 -0700

    Configure to render playbar and sidebar

--- server/index.js
+++ server/index.js
@@ -1,2 +1,18 @@
+let express = require('express');
+let request = require('request');
+let path = require('path');
+
 //server is supposed to handle a get request to it
 // when it receives a request, it sends requests to all of the services it needs to contact
+
+const PORT = 4000;
+
+let app = express();
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '../')));
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
